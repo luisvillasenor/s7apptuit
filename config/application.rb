f9e3190602199d6a247be1690cd6cd5a1dba7dce
sectureags <sectureags@gmail.com>
2016-07-13 12:54:53 -0500

    Se actualiza

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,7 @@ module S7apptuit
   class Application < Rails::Application
 
 
-    $client = Twitter::REST::Client.new do |config|
-            
-      config.consumer_key        = ENV['TWITTER_API_KEY']
-      config.consumer_secret     = ENV['TWITTER_API_SECRET']
-      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-    end
+    
 
 
     # Settings in config/environments/* take precedence over those specified here.
